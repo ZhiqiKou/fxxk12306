4f87878d480f5808873ff0db2915c3829b6880b5
zhiqi_ubuntu <935939006@qq.com>
2019-02-01 11:54:55 +0800

    筛选指定车次座位

--- fxxk12306/screening_ticket.py
+++ fxxk12306/screening_ticket.py
@@ -6,26 +6,51 @@ from search_tickets import Tickets
 
 
 class TargetTicket(object):
-    def __init__(self, trains, train_date, from_station, to_station, purpose_codes='ADULT'):
+    def __init__(self, trains):
         self.trains = trains
-        self.train_date = train_date
-        self.from_station = from_station
-        self.to_station = to_station
-        self.purpose_codes = purpose_codes
 
-    def screening(self):
-        tickets = Tickets(self.train_date, self.from_station, self.to_station)
-        # 得到所有车票
-        all_tickets = tickets.search()
-        # 得到指定车票信息
-        target_list = []
+    def screening_trains(self, all_tickets):
+        # 得到指定车次信息
+        target_dir = {}
         for ticket in all_tickets:
-            if ticket['trains'] == self.trains:
-                target_list.append(ticket)
-        print(target_list)
+            if ticket['trains'] in self.trains:
+                seat_dir = {}
+                if ticket['tdz'] != '--':
+                    seat_dir['tdz'] = ticket['tdz']
+                if ticket['ydz'] != '--':
+                    seat_dir['ydz'] = ticket['ydz']
+                if ticket['edz'] != '--':
+                    seat_dir['edz'] = ticket['edz']
+                if ticket['gjrw'] != '--':
+                    seat_dir['gjrw'] = ticket['gjrw']
+                if ticket['rw'] != '--':
+                    seat_dir['rw'] = ticket['rw']
+                if ticket['yw'] != '--':
+                    seat_dir['yw'] = ticket['yw']
+                if ticket['yz'] != '--':
+                    seat_dir['yz'] = ticket['yz']
+                if ticket['wz'] != '--':
+                    seat_dir['wz'] = ticket['wz']
+                target_dir[ticket['trains']] = seat_dir
+        print(target_dir)
+        return target_dir
+
+    def screening_seat(self, all_tickets, trains_seat_dir):
+        target_dir = self.screening_trains(all_tickets)
+        for key in trains_seat_dir:
+            print(key)
+            for seat in trains_seat_dir[key]:
+                print(target_dir[key][seat])
 
 
 if __name__ == '__main__':
     # 测试信息
-    target_ticket = TargetTicket('K270', '2019-02-01', '洛阳', '北京')
-    target_ticket.screening()
+    tickets = Tickets('2019-03-01', '上海', '北京')
+    # 打印所有车票信息
+    print(tickets)
+    # 得到所有车票列表
+    all_tickets = tickets.search()
+    # 晒选指定车次
+    target_ticket = TargetTicket(['D708', 'Z282', 'G22'])
+    # target_ticket.screening_trains(all_tickets)
+    target_ticket.screening_seat(all_tickets, {'D708': ['edz', 'rw'], 'Z282': ['rw', 'wz']})
